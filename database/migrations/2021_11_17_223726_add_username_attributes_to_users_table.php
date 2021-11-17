e7a60ed736a431841075355b86f62902f046bb8e
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-17 23:45:06 +0100

    Add migration AddUsernameAttributesToUsersTable

---
+++ database/migrations/2021_11_17_223726_add_username_attributes_to_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsernameAttributesToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $usersTable         = config('custom-user.user.table.name');
+        $usernameEnabled    = config('custom-user.attributes.username.enabled');
+
+        if ($usernameEnabled) {
+            Schema::table($usersTable, function (Blueprint $table) {
+                $table->string('username') ->nullable() ->after('email_verified_at');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $usersTable = config('custom-user.user.table.name');
+
+        Schema::table($usersTable, function (Blueprint $table) {
+            $table->dropColumn('username');
+        });
+    }
+}
