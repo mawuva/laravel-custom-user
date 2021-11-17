e61c5ec69a94273e57f4a744ee7da40921ac8958
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-17 23:36:26 +0100

    Add password checker feature in src/Actions/UpdatePasswordAction.php

--- src/Actions/UpdatePasswordAction.php
+++ src/Actions/UpdatePasswordAction.php
@@ -4,9 +4,27 @@ namespace Mawuekom\CustomUser\Actions;
 
 use Illuminate\Database\Eloquent\Model;
 use Mawuekom\CustomUser\Facades\CustomUser;
+use Mawuekom\PasswordHistory\Services\PasswordHistoryChecker;
 
 class UpdatePasswordAction
 {
+    /**
+     * @var \Mawuekom\PasswordHistory\Services\PasswordHistoryChecker
+     */
+    private $passwordHistoryChecker;
+
+    /**
+     * Create new action instance
+     *
+     * @param \Mawuekom\PasswordHistory\Services\PasswordHistoryChecker $passwordHistoryChecker
+     * 
+     * @return void
+     */
+    public function __construct(PasswordHistoryChecker $passwordHistoryChecker)
+    {
+        $this ->passwordHistoryChecker = $passwordHistoryChecker;
+    }
+    
     /**
      * Execute action
      *
@@ -17,6 +35,12 @@ class UpdatePasswordAction
      */
     public function execute(Model $model, string $password): Model
     {
+        $passwordHistoryCheckerEnabled = config('custom-user.password_history.checker');
+
+        if ($passwordHistoryCheckerEnabled) {
+            $this ->passwordHistoryChecker ->validatePassword($model, $password);
+        }
+
         $model ->password    = CustomUser::handlePassword($password);
         CustomUser::updatePasswordChangedAt($model);
         $model ->save();
