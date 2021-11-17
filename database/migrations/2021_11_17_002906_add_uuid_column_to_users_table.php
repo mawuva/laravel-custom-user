a4bfcd06ccee7139436fdc08b7bac080ce628b00
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-17 01:53:35 +0100

    Add AddUuidColumnToUsersTable migration for allow uuid support in users table

---
+++ database/migrations/2021_11_17_002906_add_uuid_column_to_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUuidColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $usersTable     = config('custom-user.user.table.name');
+        $usersTablePK   = config('custom-user.user.table.primary_key');
+
+        $uuidEnabled    = config('custom-user.uuids.enabled');
+        $uuidColumn     = config('custom-user.uuids.column');
+
+        if ($uuidEnabled && $uuidColumn !== null) {
+            Schema::table($usersTable, function (Blueprint $table)
+            use($uuidColumn, $usersTablePK) {
+                $table->uuid($uuidColumn) ->after($usersTablePK);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $usersTable     = config('custom-user.user.table.name');
+        $uuidColumn     = config('custom-user.uuids.column');
+
+        Schema::table($usersTable, function (Blueprint $table) use ($uuidColumn) {
+            $table->dropColumn($uuidColumn);
+        });
+    }
+}
