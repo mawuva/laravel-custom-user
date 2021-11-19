a6f35c1fc0555044d26200c165d4b8c3c2e7d8e2
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-19 02:23:01 +0100

    Update getUserByField in CustomUser.php by renaming variables

--- src/CustomUser.php
+++ src/CustomUser.php
@@ -61,7 +61,7 @@ class CustomUser
      */
     public function getUserByField($field, $value = null, $inTrashed = false, $columns = ['*'])
     {
-        $data = app(config('custom-user.user.model')) ->where($attribute, '=', $id);
+        $data = app(config('custom-user.user.model')) ->where($field, '=', $id);
 
         return ($inTrashed)
                     ? $data ->withTrashed() ->first($columns)
