d93808d02d20cdbf6cd56ac8fadaf36aec1c7c78
Seddor Ephraim <seddorephraim7@gmail.com>
2021-12-02 00:45:15 +0100

    Refactor: Remove resolve_key function from helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -16,34 +16,6 @@ if (!function_exists('uuid_is_enabled_and_has_column_defined')) {
     }
 }
 
-if (!function_exists('resolve_key')) {
-    /**
-     * Get key to use to make queries
-     * 
-     * @param string $config
-     * @param string $entity
-     * @param int|string $id
-     * @param bool $inTrashed
-     * 
-     * @return string|null
-     */
-    function resolve_key(string $config, string $entity, $id = null, $inTrashed = false) {
-        $model          = config($config.'.'.$entity.'.model');
-        $uuidColumn     = config($config.'.uuids.column', '_id');
-        $entityPK       = config($config.'.'.$entity.'.table.primary_key');
-
-        if (config($config.'.uuids.enabled')) {
-            return (is_the_given_id_a_uuid($uuidColumn, $id, $model, $inTrashed))
-                    ? $uuidColumn
-                    : $entityPK;
-        }
-
-        else {
-            return $entityPK;
-        }   
-    }
-}
-
 if (!function_exists('get_attribute')) {
     /**
      * Get attribute config property
