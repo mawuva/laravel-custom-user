5970f1db36b4a80e3641d5b325c7ba6c4de72154
Ephraim Seddor <seddorephraim7@gmail.com>
2022-01-29 12:20:21 +0100

    Add ChangeAttributeNameDefaultValueToNullInUsersTable

---
+++ database/migrations/2022_01_27_174620_change_attribute_name_default_value_to_null_in_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeAttributeNameDefaultValueToNullInUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $usersTable     = config('custom-user.user.table.name');
+        $nameIsOptional = config('custom-user.attributes.name');
+
+        if ($nameIsOptional) {
+            Schema::table($usersTable, function (Blueprint $table) {
+                $table->string('name')->nullable()->change();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('null_in_users', function (Blueprint $table) {
+            //
+        });
+    }
+}
